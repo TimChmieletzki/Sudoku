423c46cb17a47932339a67132786d9e3564ef548
Tim Chmieletzki <timchmieletzki@gmail.com>
2025-02-21 13:32:21 +0100

    updated favicon, logo

--- app/components/home/Navigation.tsx
+++ app/components/home/Navigation.tsx
@@ -1,9 +1,18 @@
+import Image from "next/image";
+
 export function Navigation() {
   return (
     <header className="fixed top-0 w-full border-b border-neutral-200 bg-white/80 backdrop-blur-sm">
       <nav className="mx-auto flex h-16 max-w-screen-xl items-center justify-between px-6">
         <div className="flex items-center space-x-4">
-          <span className="font-mono text-xl font-bold">v0.7</span>
+          <Image
+            src="/logo.svg"
+            alt="Sudoku Logo"
+            width={32}
+            height={32}
+            priority={true}
+          />
+          <span className="font-mono text-xl font-bold">Sudoku v0.7</span>
         </div>
         <div className="flex items-center space-x-4">
           <a
